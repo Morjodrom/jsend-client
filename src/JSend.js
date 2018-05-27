fb3d946a7c383768da3f3eabcb393c19e9f7a2e8
Dmitry <danev@htr.su>
2018-05-27 11:58:21 +0300

    Pure js docs refactoring

--- src/JSend.js
+++ src/JSend.js
@@ -1,15 +1,25 @@
+/**
+ * @class JSend
+ */
 export default class JSend {
 	/**
-	 * @param status
-	 * @param data
-	 * @param message
+	 * @param {string} status
+	 * @param {null|Array|Object} data
+	 * @param {null|String} message
 	 */
-	constructor({status, data, message}) {
-		this.status = status;
-		this.data = data || [];
-		this.message = message;
+	constructor({status, data = null, message = null}) {
+		this.status = status
+		this.data = data
+		this.message = message
 	}
 
+	/**
+	 * Utilizes string json or JS object to create a new JSend instance.
+	 * Consider usage with server requests.
+	 *
+	 * @param {string|Object} json
+	 * @return {JSend}
+	 */
 	static parse(json){
 		if(typeof json === 'string'){
 			json = JSON.parse(json)
@@ -17,19 +27,44 @@ export default class JSend {
 		return new JSend(json)
 	}
 
+	/**
+	 * Syntactic sugar to check if the JSend response is successfull
+	 *
+	 * @return {boolean}
+	 */
 	get success(){
 		return this.status === JSend.SUCCESS
 	}
 
+	/**
+	 * @return {boolean}
+	 */
 	get fail(){
 		return this.status === JSend.FAIL
 	}
 
+	/**
+	 * @return {boolean}
+	 */
 	get error(){
 		return this.status === JSend.ERROR
 	}
 }
-
-JSend.SUCCESS = 'success';
-JSend.ERROR = 'error';
-JSend.FAIL = 'fail';
+/**
+ * All went well, and (usually) some data was returned.
+ *
+ * @type {string}
+ */
+JSend.SUCCESS = 'success'
+/**
+ * An error occurred in processing the request, i.e. an exception was thrown
+ *
+ * @type {string}
+ */
+JSend.ERROR = 'error'
+/**
+ * There was a problem with the data submitted, or some pre-condition of the API call wasn't satisfied
+ *
+ * @type {string}
+ */
+JSend.FAIL = 'fail'
